b6764783b2880e7a62e13487fda731f7a0104082
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-10-19 09:36:24 +0100

    Add health check

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,13 @@
 from application import app
 from flask import Response
+from application.health import healthcheck
 
 
 @app.route('/', methods=["GET"])
 def index():
     return Response(status=200)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return healthcheck()
