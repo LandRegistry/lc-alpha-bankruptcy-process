a784dbed41765a1c5e3101ab00a022cf1333946b
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-03-18 08:44:38 +0000

    Comment out hopefully unused bits. Some extra log feedback.

--- application/process.py
+++ application/process.py
@@ -184,6 +184,8 @@ def process_entry(producer, entry):
                     "subsystem": CONFIG['APPLICATION_NAME'],
                     "type": "E"
                 })
+        else:
+            logging.info("Skip non-bankruptcy: %s", coc)
 
 
 def process(config, date):
@@ -238,71 +240,71 @@ def raise_error(producer, error):
     logging.warning('Error successfully raised.')
 
 
-def message_received(body, message):
-    logging.info("Received new registrations: %s", str(body))
-    errors = []
-
-    # TODO: only execute against relevant registrations/amendments etc.
-
-    request_uri = CONFIG['REGISTER_URI'] + '/registrations/'
-    for application in body['data']:
-        number = application['number']
-        date = application['date']
-
-        try:
-            logging.info("Processing %s", number)
-            uri = "{}{}/{}".format(request_uri, date, number)
-            response = requests.get(uri)
-
-            if response.status_code == 200:
-                logging.info("Received response 200 from /registrations")
-                registration_response = response.json()
-
-                print(registration_response)
-                if 'complex' in registration_response:
-                    # Complex Name Case...
-                    name_search_result = get_complex_name_matches(registration_response['complex']['number'])
-                else:
-                    name_search_result = get_simple_name_matches(registration_response['debtor_name'])
-
-                post_response = post_bankruptcy_search(registration_response, name_search_result)
-                if post_response.status_code == 200:
-                    logging.info("Received response 200 from legacy db ")
-                else:
-                    logging.error("Received response %d from legacy db trying to add debtor %s",
-                                  post_response.status_code, number)
-                    save_error(errors, post_response, '/debtor', number)
-            else:
-                logging.error("Received response %d from bankruptcy-registration for registration %s",
-                              response.status_code, number)
-                save_error(errors, response, '/registrations', number)
-
-        # pylint: disable=broad-except
-        except Exception as exception:
-            errors.append({
-                "registration_no": number,
-                "exception_class": type(exception).__name__,
-                "error_message": str(exception)
-            })
-
-    message.ack()
-    if len(errors) > 0:
-        raise BankruptcyProcessError(errors)
-
-
-def listen(incoming_connection, error_producer, run_forever=True):
-    logging.info('Listening for new registrations')
-
-    while True:
-        try:
-            incoming_connection.drain_events()
-        except BankruptcyProcessError as exception:
-            for error in exception.value:
-                error_producer.put(error)
-            logging.info("Error published")
-        except KeyboardInterrupt:
-            logging.info("Interrupted")
-            break
-
-        if not run_forever:
-            break
+# def message_received(body, message):
+#     logging.info("Received new registrations: %s", str(body))
+#     errors = []
+#
+#     # TODO: only execute against relevant registrations/amendments etc.
+#
+#     request_uri = CONFIG['REGISTER_URI'] + '/registrations/'
+#     for application in body['data']:
+#         number = application['number']
+#         date = application['date']
+#
+#         try:
+#             logging.info("Processing %s", number)
+#             uri = "{}{}/{}".format(request_uri, date, number)
+#             response = requests.get(uri)
+#
+#             if response.status_code == 200:
+#                 logging.info("Received response 200 from /registrations")
+#                 registration_response = response.json()
+#
+#                 print(registration_response)
+#                 if 'complex' in registration_response:
+#                     # Complex Name Case...
+#                     name_search_result = get_complex_name_matches(registration_response['complex']['number'])
+#                 else:
+#                     name_search_result = get_simple_name_matches(registration_response['debtor_name'])
+#
+#                 post_response = post_bankruptcy_search(registration_response, name_search_result)
+#                 if post_response.status_code == 200:
+#                     logging.info("Received response 200 from legacy db ")
+#                 else:
+#                     logging.error("Received response %d from legacy db trying to add debtor %s",
+#                                   post_response.status_code, number)
+#                     save_error(errors, post_response, '/debtor', number)
+#             else:
+#                 logging.error("Received response %d from bankruptcy-registration for registration %s",
+#                               response.status_code, number)
+#                 save_error(errors, response, '/registrations', number)
+#
+#         # pylint: disable=broad-except
+#         except Exception as exception:
+#             errors.append({
+#                 "registration_no": number,
+#                 "exception_class": type(exception).__name__,
+#                 "error_message": str(exception)
+#             })
+#
+#     message.ack()
+#     if len(errors) > 0:
+#         raise BankruptcyProcessError(errors)
+#
+#
+# def listen(incoming_connection, error_producer, run_forever=True):
+#     logging.info('Listening for new registrations')
+#
+#     while True:
+#         try:
+#             incoming_connection.drain_events()
+#         except BankruptcyProcessError as exception:
+#             for error in exception.value:
+#                 error_producer.put(error)
+#             logging.info("Error published")
+#         except KeyboardInterrupt:
+#             logging.info("Interrupted")
+#             break
+#
+#         if not run_forever:
+#             break
